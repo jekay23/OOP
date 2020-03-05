9eeef4d3670d3ccd7dac6dda3cb61d0f2f435db8
Evgeny Yatsko <evgyatsko@gmail.com>
2020-03-05 15:40:29 +0700

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Евгений Яцко on 05.03.2020.
+//  Copyright © 2020 Евгений Яцко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
